ea5e2bea4b518e030285a50869dc8a3278842ab2
Unn2007 <unn2007@i.ua>
2024-11-04 09:19:36 +0200

    Fix_punkt_8_in_modal_js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,26 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    links: document.querySelectorAll('.nav-link'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  console.log(refs.links);
+
+  refs.links.forEach(link => {
+    if (link) {
+      link.addEventListener('click', closeModal);
     }
-  })();
+  });
+
+  function closeModal() {
+    refs.modal.classList.add('is-hidden');
+  }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
